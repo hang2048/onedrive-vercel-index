845a6def8a0c4df9af23acf7b04e61d89d2e9b1a
HANG <105864471+hang2048@users.noreply.github.com>
2022-08-01 11:54:01 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/hang2048/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">ONE-Index</a>. Made with ❤ by HANG.',
+    'Powered by <a href="https://github.com/hang2048/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">ONE-Index</a>. Made with ❤ by HANG＆XU·2GOU.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
